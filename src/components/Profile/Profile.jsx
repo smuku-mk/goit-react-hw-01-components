fd64ae061d85c35e974ea28e0a8cf8b21bfe02e7
Mateusz Kubiak <smuku.mk@gmail.com>
2023-06-12 20:50:17 +0200

    task_01

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import './Profile.css';
+import user from './user.json';
+
+export const Profile = () => {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={user.avatar} alt="User avatar" className="avatar" />
+        <p className="name">{user.username}</p>
+        <p className="tag">@{user.tag}</p>
+        <p className="location">{user.location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{user.stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{user.stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{user.stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
